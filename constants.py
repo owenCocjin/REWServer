752a6bf3f0860e658261f298e75bf6fdb684ff78
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-18 23:59:07 -0500

    Basic web server now working!

---
+++ constants.py
@@ -0,0 +1,8 @@
+## Author:  Owen Cocjin
+## Version: 0.1
+## Date:    2020.12.18
+## Description:  Manage the web server's socket connections
+## Notes:
+ADDR='0.0.0.0'
+PORT=8080
+ROOT="HTML"
---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+## Author:  Owen Cocjin
+## Version: 0.1
+## Date:    2020.12.18
+## Description:  Main script
+## Notes:
+import serverdriver
+from progmenu import menu
+from constants import *
+
+#------------#
+#    MAIN    #
+#------------#
+def main():
+	print("<|X> Setting up socket...")
+	serv_sock=serverdriver.socksetup(ADDR, PORT)
+	print("<|X> Starting server...")
+	while True:
+		serverdriver.sockprocess(serv_sock)
+		print()
+
+
+if __name__=="__main__":
+	try:
+		main()
+	except KeyboardInterrupt as e:
+		print("\033[K")
