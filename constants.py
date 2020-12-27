258e3db0c2849561bf213dd065d07f2066c87f9b
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-26 21:29:58 -0500

    Added THREAD; Changed PIPES

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 ## Author:  Owen Cocjin
-## Version: 0.1
-## Date:    2020.12.18
+## Version: 0.2
+## Date:    2020.12.26
 ## Description:  Uses a dict named "GLOBE" to manage constants
 ## Notes:
 ##    - GLOBE is a dict used for globals that may change across all files
@@ -8,6 +9,7 @@
 ## Update Notes:
 GLOBE={
 "ADDR":"0.0.0.0",
+"PIPES":["AJAX/cts.bridge", "AJAX/stc.bridge"],  #[client to server, server to client]
 "PORT":8080,
-"ROOT":"HTML"
-}
+"ROOT":"HTML",
+"THREADS":[]}
